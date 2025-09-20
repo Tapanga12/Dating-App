17cc5123c47a26861b56731157bfa03aee2369e2
Tapanga Witt <91804327+Tapanga12@users.noreply.github.com>
2025-09-20 19:47:22 -0400

    Set up Flask app with SQLAlchemy and LoginManager

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 import datetime
 from typing import List, Optional
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from sqlalchemy import String, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
 from model import db, User, Match, BlockedUser  # Import models from model.py
 
-
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datingapp.db'
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-db.init_app(app)  # Initialize SQLAlchemy with the Flask app
+db.init_app(app)
 
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
- 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+# import routes (if in a separate file)
+import routes  
 
 if __name__ == '__main__':
-    with app.app_context():  # Makes sure to be in app context when calling db.create_all()
-        db.create_all()  # Create the tables defined in model
-    app.run(debug=True) 
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
